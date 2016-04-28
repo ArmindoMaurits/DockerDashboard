380fec63c0a32c1a38d0ecf6a9e1826958b3bc89
ArmindoMaurits <a.r.maurits@gmail.com>
2016-04-28 14:54:55 +0200

    JSON get via httprequest

--- DockerTestAma/src/DockerTestAma/Controllers/HomeController.cs
+++ DockerTestAma/src/DockerTestAma/Controllers/HomeController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using Microsoft.AspNet.Mvc;
 using DockerTestAma.Models;
-using Newtonsoft.Json;
+using System.IO;
+using System.Net;
 
 namespace DockerTestAma.Controllers
 {
@@ -10,23 +11,20 @@ namespace DockerTestAma.Controllers
     {
         public IActionResult Index()
         {
-
+            List<Container> containers = getContainers();
+            
             return View();
         }
 
-        [HttpPost]
-        public JsonResult postToGetContainers([FromBody]Object containers)
-        {
-            //List<Container> containerList = getContainers(containers);
-
-            return Json("OK");
-        }
-
-        private List<Container> getContainers(string[] containerArray)
+        private List<Container> getContainers()
         {
             List<Container> containerList = new List<Container>();
             String url = "http://amaurits.nl/get/containers.json";
+            Console.WriteLine("url: " + url);
+            string response = GetHtmlPage(url);
+            Console.WriteLine("response: " + response);
 
+            //TODO: Parse json
             return containerList;
         }
 
@@ -35,7 +33,30 @@ namespace DockerTestAma.Controllers
             List<Image> imageList = new List<Image>();
             String url = "http://amaurits.nl/get/images.json";
 
+            string response = GetHtmlPage(url);
             return imageList;
+        }
+
+        static string GetHtmlPage(string strURL)
+        {
+            String strResult;
+            WebResponse objResponse;
+
+            WebRequest objRequest = HttpWebRequest.Create(strURL);
+            objResponse = objRequest.GetResponse();
+            using (StreamReader sr = new StreamReader(objResponse.GetResponseStream()))
+            {
+                strResult = sr.ReadToEnd();
+                sr.Close();
+            }
+            return strResult;
+        }
+
+        static string parseJson(string data)
+        {
+            string json = "";
+
+            return json;
         }
     }
 }
